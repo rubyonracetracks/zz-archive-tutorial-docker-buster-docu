b28d8cd89c6921d7cc2bb9063013922c52cd2ba5
Jason Hsu <jhsu802701@jasonhsu.com>
2019-05-13 20:36:26 +0000

    Removed users showcase section

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,17 +8,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
@@ -40,9 +29,6 @@ const siteConfig = {
     {doc: 'doc1', label: 'Docs'},
     {page: 'help', label: 'Help'}
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* Colors for website */
   colors: {
